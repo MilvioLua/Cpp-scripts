a4c9cf77d38fe26a0431d01c06a0041b45c94c7e
Milvio LuA <milviolua@gmail.com>
2020-05-20 00:08:59 +0200

    addedunhide
    addedunhide

--- Console/console/main.cpp
+++ Console/console/main.cpp
@@ -2,22 +2,33 @@
 
 using namespace std;
 
+auto secword = 1;
+
 string hide(){
-    auto Secword = 1;
     string word;
-    cout << "encrypt word";
+    cout << "encrypt word" <<endl;
     cin>> word;
     word[0] = 'x';
     word[2] = 'k';
     cout << word;
     return word;
-
 };
 
+string givenWord = hide();
+
+string uhide(){
+    if (secword ==1){
+            cout << givenWord << endl;
+    }
+    else {
+            cout << "wrong password try again"<< endl;
+        }
+
+  }
 
 int main()
 {
     hide();
-
+    uhide();
     return 0;
 }
