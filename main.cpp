62e807f80aa781403fe0d53f1db39c9ccc2b8ca1
Milvio LuA <milviolua@gmail.com>
2020-05-19 14:25:51 +0200

    add math
    add math

--- main.cpp
+++ main.cpp
@@ -6,16 +6,10 @@ using namespace std;
 int main()
 {
 
-    int num1, num2, operation, result ;
 
-    cout <<"Enter First number :";
-    cin >> num1;
-    cout << "Enter math operator:";
-    cin >> operation;
-    cout << "enter second number :";
-    cin >> num2;
-
-    cout << num1 + num2;
+    cout << "Roses are Red" << endl;
+    cout << "violets areblue" << endl;
+    cout << "I love you " << endl;
 
 
 
