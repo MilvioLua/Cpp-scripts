18009c9c655e89371272a4a6f08f1acc94843aac
Milvio LuA <milviolua@gmail.com>
2020-05-19 14:20:16 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+    int num1, num2, operation, result ;
+
+    cout <<"Enter First number :";
+    cin >> num1;
+    cout << "Enter math operator:";
+    cin >> operation;
+    cout << "enter second number :";
+    cin >> num2;
+
+    cout << num1 + num2;
+
+
+
+
+    return 0;
+}
